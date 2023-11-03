833c83478b3e57c77f3175f79944adcce08466fe
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2023-11-03 19:26:57 +0530

    more ui shit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# rbf
-Relative Bus Factor
+# relative 🚌 factor
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig( {
+  base: './',
+  build: {
+    outDir: 'docs'
+  },
   server: {
     port: 3000
   }
